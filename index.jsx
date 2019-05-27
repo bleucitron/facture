cbdf47cb5e1d022482229c01f43315d558ae3d82
Romain Crestey <rcrestey@gmail.com>
2019-05-27 15:58:48 +0200

    feat: adds DA number in Footer

--- index.jsx
+++ index.jsx
@@ -172,11 +172,12 @@ const Payment = ({ bank, iban, bic}) => (
 );
 
 const Provider = ({
-  description, email, phone, address, zip, city, siret, ...names
+  description, email, phone, address, zip, city, siret, da, da_place, ...names
 }) => (
   <div className='provider'>
     <div className='name'>{ `${getFullName(names)} \n { ${ description } }` }</div>
     <div className='siret'><span>SIRET</span>{ siret }</div>
+    <div className='formation'>{`DA de formation ${ da } (${ da_place })`}</div>
     <div className='email'>{ email }</div>
     <div className='phone'>{ phone }</div>
     <div className='address'>{ `${address} ${zip} ${city}` }</div>
