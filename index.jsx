26208014b987acf49d4c3447a6d8f87c805af442
Romain Crestey <rcrestey@gmail.com>
2019-06-01 15:24:06 +0200

    feat: uses inquirer to ask questions

--- index.jsx
+++ index.jsx
@@ -6,73 +6,113 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { DateTime } from 'luxon';
 import pdf from 'html-pdf';
+import inquirer from 'inquirer';
+import colors from 'colors';
 
 import Invoice from './Invoice';
 
-program
-  .version('0.1.0')
-  .option('-o, --output-dir <output-dir>', 'Output Directory')
-  .option('-c, --client <client>', 'Customer')
-  .option('-d, --debug', 'Debug mode')
-  .parse(process.argv);
-
-if (!program.client)
-  program.client = 'HUM';
-
-const html = fs.readFileSync('./base.html');
 const provider = yaml.safeLoad(fs.readFileSync('./data/provider.yaml'), 'utf8');
 const clients = yaml.safeLoad(fs.readFileSync('./data/clients.yaml'), 'utf8');
 const items = yaml.safeLoad(fs.readFileSync('./data/items.yaml'), 'utf8');
 const invoices = yaml.safeLoad(fs.readFileSync('./data/invoices.yaml'), 'utf8') || [];
 
-const dateArray = DateTime.local().toISO().split('T')[0].split('-');
+program
+  .version('0.1.0')
+  .option('-o, --output-dir <output-dir>', 'Output Directory')
+  .option('-d, --debug', 'Debug mode')
+  .parse(process.argv);
 
-const client = clients[program.client];
+inquirer
+.prompt([{
+  type: 'list',
+  name: 'client',
+  message: 'What is the client?',
+  choices: Object.values(clients).map(client => ({
+    name: client.name,
+    value: client.code
+  })),
+  filter: code => clients[code]
+}, {
+  type: 'confirm',
+  name: 'ok',
+  message: () => {
+    items.forEach((item, i) => {
+      const { description, quantity, unitPrice } = item
 
-const id = invoices.length + 1;
-const number = invoices.reduce((acc, invoice) => {
-  if (invoice.clientCode === client.code)
-    return acc + 1;
-  return acc;
-}, 1);
+      console.log();
+      const title = `--- Item ${i + 1} ---`;
+      const qty = quantity.toString().bold;
+      const price = `${unitPrice.toString()}€`.bold;
+      console.log(`${title.bold} qty: ${qty} price: ${price}`);
+      console.log();
+      console.log(description.replace('\n\n', '\n'));
+    });
 
-const label = [...dateArray.slice(0, 2), client.code, `F${number}`].join('_');
+    const total = items.reduce((acc, { unitPrice, quantity }) => acc + unitPrice * quantity, 0);
+    const qty = items.length.toString().bold;
+    const price = `${total.toString()}€`.bold;
+    console.log(`This invoice has ${ qty } items, for a total of ${ price }.`)
 
-const invoice = {
-  id,
-  label,
-  date: dateArray.reverse().join('/'),
-  clientCode: client.code,
-  priceHT: items.reduce((acc, { unitPrice, quantity }) => acc + unitPrice * quantity, 0)
-};
+    return 'Is this ok?';
+  },
+  default: true,
+  filter: code => clients[code]
+}])
+.then(({ client, ok }) => {
+  if (!ok) return;
 
-invoices.push(invoice);
+  const dateArray = DateTime.local().toISO().split('T')[0].split('-');
 
-const dom = new JSDOM(html, { resources: 'usable'});
-const document = dom.window.document;
+  const id = invoices.length + 1;
+  const number = invoices.reduce((acc, invoice) => {
+    if (invoice.clientCode === client.code)
+      return acc + 1;
+    return acc;
+  }, 1);
 
-document.body.innerHTML = ReactDOMServer.renderToStaticMarkup(
-  <Invoice provider={provider} items={items} client={client} invoice={invoice} />
-);
+  const label = [...dateArray.slice(0, 2), client.code, `F${number}`].join('_');
 
-var options = {
-  format: 'A4',
-  // footer: {
-  //   contents: {
-  //     height: '10px',
-  //     default: ReactDOMServer.renderToStaticMarkup(<Footer provider={provider} invoice={invoice} />),
-  //   }
-  // },
-};
+  console.log();
+  console.log(`Generating invoice #${id}, ${label.bold}...`);
 
-fs.writeFileSync('./build/index.html', dom.serialize());
+  const html = fs.readFileSync('./base.html');
 
-if (!program.debug)
-  fs.writeFileSync('./data/invoices.yaml', yaml.safeDump(invoices));
+  const invoice = {
+    id,
+    label,
+    date: dateArray.reverse().join('/'),
+    clientCode: client.code,
+    priceHT: items.reduce((acc, { unitPrice, quantity }) => acc + unitPrice * quantity, 0)
+  };
 
-const outputDir = program.outputDir || '.';
+  invoices.push(invoice);
 
-pdf.create(dom.serialize(), options).toFile(`${outputDir}/${label}.pdf`, function (err, res) {
-  if (err) return console.log(err);
-  console.log(res);
+  const dom = new JSDOM(html, { resources: 'usable' });
+  const document = dom.window.document;
+
+  document.body.innerHTML = ReactDOMServer.renderToStaticMarkup(
+    <Invoice provider={provider} items={items} client={client} invoice={invoice} />
+  );
+
+  var options = {
+    format: 'A4',
+    // footer: {
+    //   contents: {
+    //     height: '10px',
+    //     default: ReactDOMServer.renderToStaticMarkup(<Footer provider={provider} invoice={invoice} />),
+    //   }
+    // },
+  };
+
+  fs.writeFileSync('./build/index.html', dom.serialize());
+
+  if (!program.debug)
+    fs.writeFileSync('./data/invoices.yaml', yaml.safeDump(invoices));
+
+  const outputDir = program.outputDir || '.';
+
+  pdf.create(dom.serialize(), options).toFile(`${outputDir}/${label}.pdf`, function (err, res) {
+    if (err) return console.log(err);
+    console.log(res.filename.green);
+  });
 });
