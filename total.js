832b6cbd7c468c4bfab9e4c7c58a42e7545f3f78
Romain Crestey <rcrestey@gmail.com>
2020-02-26 18:16:51 +0100

    feat(total): add TVA in logs

--- total.js
+++ total.js
@@ -1,58 +1,13 @@
 import { readFileSync } from 'fs';
 import { safeLoad } from 'js-yaml';
-import { DateTime } from 'luxon';
-import chalk from 'chalk';
 
-import { formatPrice } from './utils';
-
-function evaluate(amount, rate) {
-  return Math.round((amount * rate) / 100);
-}
-
-function logAmount(text, value) {
-  const amount = chalk.blue(`${formatPrice(value)}`);
-  const cotisation = chalk.grey(`5,5%: ${evaluate(value, 5.5)}€`);
-  const impots = chalk.grey(`2,2%: ${evaluate(value, 2.2)}€`);
-
-  console.log(`${text}: ${amount} / ${cotisation} / ${impots}`);
-}
-
-function logQuarter(quarter, year, value) {
-  const amount = chalk.green(`${formatPrice(value)}`);
-
-  console.log(`${year} Q${quarter}: ${amount}`);
-  console.log();
-}
-
-function logYear(year, value) {
-  const amount = chalk.green(`${formatPrice(value)}`);
-
-  console.log(`${year}: ${amount}`);
-  console.log();
-}
-
-function parseDate(date) {
-  return DateTime.fromFormat(date, 'dd/MM/yyyy');
-}
-
-function makeDateKeys(all) {
-  const first = parseDate(all[0].date);
-  const last = parseDate(all[all.length - 1].date);
-
-  let current = first;
-  let currentKey = first.toFormat('yyyy/MM');
-  const lastKey = last.toFormat('yyyy/MM');
-
-  const keys = [];
-
-  while (currentKey <= lastKey) {
-    keys.push(current.toFormat('MM/yyyy'));
-    current = current.plus({ months: 1 });
-    currentKey = current.toFormat('yyyy/MM');
-  }
-
-  return keys;
-}
+import {
+  parseDate,
+  logAmount,
+  logQuarter,
+  logYear,
+  makeDateKeys,
+} from './utils';
 
 const invoices = safeLoad(readFileSync('./data/invoices.yaml'), 'utf8');
 const keys = makeDateKeys(invoices);
@@ -72,8 +27,6 @@ const entries = keys.map(key => {
 
   return [key, amount];
 });
-
-// console.log('All by month', Object.fromEntries(entries));
 
 let curQuarter;
 let curYear;
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,6 @@
 import inquirer from 'inquirer';
+import { DateTime } from 'luxon';
+import chalk from 'chalk';
 import { formatDefaultLocale } from 'd3-format';
 import locale from 'd3-format/locale/fr-FR';
 
@@ -55,6 +57,74 @@ export function prompt(clients, items) {
   ]);
 }
 
-export function formatPrice(price) {
+function formatPrice(price) {
   return format('($,.2f')(price);
+}
+
+function evaluate(amount, rate) {
+  return Math.round((amount * rate) / 100);
+}
+
+function calculateRates(value, _chalk = chalk) {
+  // const cotisation = chalk.grey(`5,5%: ${evaluate(value, 5.5)}€`);
+  const cotisation = _chalk.grey(`16,5%: ${evaluate(value, 16.5)}€`);
+  const impots = _chalk.grey(`2,2%: ${evaluate(value, 2.2)}€`);
+  const tva = _chalk.yellow(`20%: ${evaluate(value, 20)}€`);
+
+  return {
+    cotisation,
+    impots,
+    tva,
+  };
+}
+
+export function parseDate(date) {
+  return DateTime.fromFormat(date, 'dd/MM/yyyy');
+}
+
+export function logAmount(text, value) {
+  const { cotisation, impots, tva } = calculateRates(value);
+  const amount = chalk.blue(`${formatPrice(value)}`);
+
+  console.log(`${text}: ${amount} / ${cotisation} / ${impots} / ${tva}`);
+}
+
+export function logQuarter(quarter, year, value) {
+  const { cotisation, impots, tva } = calculateRates(value, chalk.bold);
+  const amount = chalk.bold.green(`${formatPrice(value)}`);
+
+  console.log(
+    `${year} Q${quarter}: ${amount} / ${cotisation} / ${impots} / ${tva}`,
+  );
+  console.log();
+}
+
+export function logYear(year, value) {
+  const { cotisation, impots, tva } = calculateRates(
+    value,
+    chalk.bold.underline,
+  );
+  const amount = chalk.bold.underline.green(`${formatPrice(value)}`);
+
+  console.log(`${year}: ${amount} / ${cotisation} / ${impots} / ${tva}`);
+  console.log();
+}
+
+export function makeDateKeys(all) {
+  const first = parseDate(all[0].date);
+  const last = parseDate(all[all.length - 1].date);
+
+  let current = first;
+  let currentKey = first.toFormat('yyyy/MM');
+  const lastKey = last.toFormat('yyyy/MM');
+
+  const keys = [];
+
+  while (currentKey <= lastKey) {
+    keys.push(current.toFormat('MM/yyyy'));
+    current = current.plus({ months: 1 });
+    currentKey = current.toFormat('yyyy/MM');
+  }
+
+  return keys;
 }
